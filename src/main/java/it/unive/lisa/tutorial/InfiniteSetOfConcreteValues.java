891547995230e21c3a308f29537c8f11718a40f5
Pietro Ferrara <pietro.ferrara@gmail.com>
2026-03-19 18:02:10 +0100

    TME 12: enlargissement sur des ensembles des interes, premiere partie intervalles

---
+++ src/main/java/it/unive/lisa/tutorial/InfiniteSetOfConcreteValues.java
@@ -0,0 +1,130 @@
+package it.unive.lisa.tutorial;
+
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.SemanticOracle;
+import it.unive.lisa.analysis.nonrelational.value.BaseNonRelationalValueDomain;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.Constant;
+import it.unive.lisa.symbolic.value.operator.*;
+import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
+import it.unive.lisa.util.representation.StringRepresentation;
+import it.unive.lisa.util.representation.StructuredRepresentation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class InfiniteSetOfConcreteValues implements BaseNonRelationalValueDomain<InfiniteSetOfConcreteValues> {
+    private static final int MAX_ELEMENTS = 10;
+    private Set<Integer> values;
+    private boolean isTop;
+    private static InfiniteSetOfConcreteValues top, bottom;
+
+    static {
+        top = new InfiniteSetOfConcreteValues(true);
+        bottom = new InfiniteSetOfConcreteValues(false);
+    }
+
+    private InfiniteSetOfConcreteValues(boolean isTop) {
+        this.isTop = isTop;
+    }
+
+    private InfiniteSetOfConcreteValues(Set<Integer> values) throws SemanticException {
+        this.values = values;
+    }
+
+    public InfiniteSetOfConcreteValues(int i) {
+        values = new HashSet<>();
+        values.add(Integer.valueOf(i));
+    }
+
+
+    @Override
+    public InfiniteSetOfConcreteValues lubAux(InfiniteSetOfConcreteValues other) throws SemanticException {
+        if(this.isBottom())
+            return other;
+        if(other.isBottom())
+            return this;
+        if(this.isTop() || other.isTop())
+            return this.top();
+        Set<Integer> s = new HashSet<>(this.values);
+        s.addAll(other.values);
+        return new InfiniteSetOfConcreteValues(s);
+
+    }
+
+    @Override
+    public InfiniteSetOfConcreteValues wideningAux(InfiniteSetOfConcreteValues other) throws SemanticException {
+        if(this.isBottom())
+            return other;
+        if(other.isBottom())
+            return this;
+        if(this.isTop() || other.isTop())
+            return this.top();
+        if(other.values.size() > MAX_ELEMENTS)
+            return this.top();
+        else return this.lubAux(other);
+    }
+
+    @Override
+    public boolean lessOrEqualAux(InfiniteSetOfConcreteValues other) throws SemanticException {
+        if(this.isTop())
+            return other.isTop();
+        return other.values.containsAll(this.values);
+    }
+
+    @Override
+    public InfiniteSetOfConcreteValues top() {
+        return top;
+    }
+
+    @Override
+    public InfiniteSetOfConcreteValues bottom() {
+        return bottom;
+    }
+
+    @Override
+    public StructuredRepresentation representation() {
+        if(isTop) return new StringRepresentation("T");
+        if(values.isEmpty()) return new StringRepresentation("_|_");
+        String value = "";
+        for(Integer i : values)
+            value += String.valueOf(i) + ";";
+        return new StringRepresentation(value);
+    }
+
+    @Override
+    public InfiniteSetOfConcreteValues evalNonNullConstant(Constant constant, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+        Object value = constant.getValue();
+        if(value instanceof Integer) {
+            return new InfiniteSetOfConcreteValues((Integer) value);
+        }
+        return BaseNonRelationalValueDomain.super.evalNonNullConstant(constant, pp, oracle);
+    }
+
+    @Override
+    public InfiniteSetOfConcreteValues evalBinaryExpression(BinaryOperator operator, InfiniteSetOfConcreteValues left, InfiniteSetOfConcreteValues right, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+        if(operator instanceof ArithmeticOperator) {
+            if(left.isBottom() || right.isBottom())
+                return this.bottom();
+            if(left.isTop() || right.isTop())
+                return this.top();
+            HashSet<Integer> s = new HashSet<>();
+            for(Integer lvalue : left.values)
+                for(Integer rvalue : right.values) {
+                    if (operator instanceof AdditionOperator)
+                        s.add(lvalue + rvalue);
+                    else if (operator instanceof SubtractionOperator)
+                        s.add(lvalue - rvalue);
+                    else if (operator instanceof MultiplicationOperator)
+                        s.add(lvalue * rvalue);
+                    else if (operator instanceof DivisionOperator) {
+                        if(rvalue != 0)
+                            s.add(lvalue / rvalue);
+                    }
+                    else throw new SemanticException("Unsupported operator");
+                }
+            return new InfiniteSetOfConcreteValues(s);
+        }
+        return BaseNonRelationalValueDomain.super.evalBinaryExpression(operator, left, right, pp, oracle);
+    }
+}
---
+++ src/main/java/it/unive/lisa/tutorial/Intervalles.java
@@ -0,0 +1,186 @@
+package it.unive.lisa.tutorial;
+
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.SemanticOracle;
+import it.unive.lisa.analysis.nonrelational.value.BaseNonRelationalValueDomain;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.Constant;
+import it.unive.lisa.symbolic.value.operator.*;
+import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
+import it.unive.lisa.util.representation.StringRepresentation;
+import it.unive.lisa.util.representation.StructuredRepresentation;
+
+import java.util.HashSet;
+
+public class Intervalles implements BaseNonRelationalValueDomain<Intervalles> {
+    private IntOrInf left, right;
+    private final static Intervalles TOP, BOTTOM;
+
+    static {
+        TOP = new Intervalles(new IntOrInf(false), new IntOrInf(true));
+        BOTTOM = new Intervalles(new IntOrInf(1), new IntOrInf(-1));
+    }
+
+    public Intervalles(int value) {
+        this.left = new IntOrInf(value);
+        this.right = new IntOrInf(value);
+    }
+
+    private Intervalles(IntOrInf left, IntOrInf right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public Intervalles lubAux(Intervalles other) throws SemanticException {
+        if(this.isBottom())
+            return other;
+        if(other.isBottom())
+            return this;
+        if (this.isTop() || other.isTop())
+            return this.top();
+        return new Intervalles(min(this.left, other.left), max(this.right, other.right));
+    }
+
+    private static IntOrInf min(IntOrInf left, IntOrInf right) {
+        if(left.isMinusInf() || right.isMinusInf())
+            return new IntOrInf(false);
+        if(left.isPlusInf())
+            return right;
+        if(right.isPlusInf())
+            return left;
+        return new IntOrInf(Math.min(left.value, right.value));
+    }
+
+    private static IntOrInf max(IntOrInf left, IntOrInf right) {
+        if(left.isPlusInf() || right.isPlusInf())
+            return new IntOrInf(true);
+        if(left.isMinusInf())
+            return right;
+        if(right.isMinusInf())
+            return left;
+        return new IntOrInf(Math.max(left.value, right.value));
+    }
+    @Override
+    public boolean lessOrEqualAux(Intervalles other) throws SemanticException {
+        return other.left.lessOrEqual(this.left) &&
+                this.right.lessOrEqual(other.right);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Intervalles) {
+            if(this.left.equals(((Intervalles) obj).left) &&
+                    this.right.equals(((Intervalles) obj).right))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "["+this.left.toString()+".."+this.right.toString()+"]";
+    }
+
+    @Override
+    public Intervalles top() {
+        return TOP;
+    }
+
+    @Override
+    public Intervalles bottom() {
+        return BOTTOM;
+    }
+
+    @Override
+    public StructuredRepresentation representation() {
+        return new StringRepresentation(this.toString());
+    }
+
+    @Override
+    public Intervalles evalNonNullConstant(Constant constant, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+        if(constant.getValue() instanceof Integer)
+            return new Intervalles(((Integer) constant.getValue()).intValue());
+        return BaseNonRelationalValueDomain.super.evalNonNullConstant(constant, pp, oracle);
+    }
+
+    @Override
+    public Intervalles evalBinaryExpression(BinaryOperator operator, Intervalles left, Intervalles right, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+        if(operator instanceof ArithmeticOperator) {
+            if (left.isBottom() || right.isBottom())
+                return this.bottom();
+            HashSet<Integer> s = new HashSet<>();
+            if (operator instanceof AdditionOperator)
+                return new Intervalles(add(left.left, right.left), add(left.right, right.right));
+            //else throw new SemanticException("Unsupported operator");
+        }
+        return BaseNonRelationalValueDomain.super.evalBinaryExpression(operator, left, right, pp, oracle);
+    }
+    private static IntOrInf add(IntOrInf left, IntOrInf right) throws SemanticException {
+        if(left.isPlusInf() && right.isMinusInf())
+            throw new SemanticException("Cannot add minus and plus infinite");
+        if(right.isPlusInf() && left.isMinusInf())
+            throw new SemanticException("Cannot add minus and plus infinite");
+        if(left.isPlusInf() || right.isPlusInf())
+            return new IntOrInf(true);
+        if(left.isMinusInf() || right.isMinusInf())
+            return new IntOrInf(false);
+        return new IntOrInf(left.value+right.value);
+    }
+    static class IntOrInf {
+
+
+        Integer value;
+        boolean isPlusInf;
+        IntOrInf(int value) {
+            this.value = Integer.valueOf(value);
+            this.isPlusInf = false;
+        }
+        IntOrInf(boolean isPlusInf) {
+            this.isPlusInf = isPlusInf;
+            this.value = null;
+        }
+        boolean isMinusInf() {
+            return value==null && !isPlusInf;
+        }
+        boolean isPlusInf() {
+            return value==null && isPlusInf;
+        }
+
+        public boolean lessOrEqual(IntOrInf left) {
+            if(this.isMinusInf() || left.isPlusInf())
+                return true;
+            if(this.isPlusInf() || left.isMinusInf())
+                return false;
+            return this.value<=left.value;
+        }
+
+        @Override
+        public String toString() {
+            if(this.isPlusInf())
+                return "+inf";
+            if(isMinusInf())
+                return "-inf";
+            return value.toString();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(obj instanceof IntOrInf) {
+                if(this.isPlusInf() && ((IntOrInf) obj).isPlusInf())
+                    return true;
+                if(this.isMinusInf() && ((IntOrInf) obj).isMinusInf())
+                    return true;
+                if(this.value!=null && ((IntOrInf) obj).value!=null)
+                    return this.value.equals(((IntOrInf) obj).value);
+            }
+            return false;
+        }
+    }
+
+}
