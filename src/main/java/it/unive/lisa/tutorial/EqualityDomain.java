384b151a17fce83aa00aa02d8b14cc1aa6059c03
rboudrouss <r.boudrouss@gmail.com>
2026-04-02 20:49:13 +0200

    feat: added back the unimplemented functions

--- src/main/java/it/unive/lisa/tutorial/EqualityDomain.java
+++ src/main/java/it/unive/lisa/tutorial/EqualityDomain.java
@@ -66,4 +66,86 @@ public class EqualityDomain extends FunctionalLattice<EqualityDomain, Identifier
         }
     }
 
+    @Override
+    public EqualityDomain assign(Identifier id, ValueExpression expression, ProgramPoint pp, SemanticOracle oracle)
+            throws SemanticException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'assign'");
+    }
+
+    @Override
+    public EqualityDomain smallStepSemantics(ValueExpression expression, ProgramPoint pp, SemanticOracle oracle)
+            throws SemanticException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'smallStepSemantics'");
+    }
+
+    @Override
+    public EqualityDomain assume(ValueExpression expression, ProgramPoint src, ProgramPoint dest, SemanticOracle oracle)
+            throws SemanticException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'assume'");
+    }
+
+    @Override
+    public boolean knowsIdentifier(Identifier id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'knowsIdentifier'");
+    }
+
+    @Override
+    public EqualityDomain forgetIdentifier(Identifier id) throws SemanticException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'forgetIdentifier'");
+    }
+
+    @Override
+    public EqualityDomain forgetIdentifiersIf(Predicate<Identifier> test) throws SemanticException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'forgetIdentifiersIf'");
+    }
+
+    @Override
+    public Satisfiability satisfies(ValueExpression expression, ProgramPoint pp, SemanticOracle oracle)
+            throws SemanticException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'satisfies'");
+    }
+
+    @Override
+    public EqualityDomain pushScope(ScopeToken token) throws SemanticException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pushScope'");
+    }
+
+    @Override
+    public EqualityDomain popScope(ScopeToken token) throws SemanticException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'popScope'");
+    }
+
+    @Override
+    public boolean lessOrEqual(EqualityDomain other) throws SemanticException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'lessOrEqual'");
+    }
+
+    @Override
+    public EqualityDomain lub(EqualityDomain other) throws SemanticException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'lub'");
+    }
+
+    @Override
+    public EqualityDomain top() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'top'");
+    }
+
+    @Override
+    public EqualityDomain bottom() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'bottom'");
+    }
+
 }
