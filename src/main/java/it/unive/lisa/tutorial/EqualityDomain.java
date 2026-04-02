9f2681da83888082b5e5274603a5206e0c5fabe5
rboudrouss <r.boudrouss@gmail.com>
2026-04-02 20:48:23 +0200

    feat: use Functionnal Lattice & identifier so i don't reimplement things

--- src/main/java/it/unive/lisa/tutorial/EqualityDomain.java
+++ src/main/java/it/unive/lisa/tutorial/EqualityDomain.java
@@ -1,10 +1,15 @@
 package it.unive.lisa.tutorial;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Predicate;
 
 import it.unive.lisa.analysis.ScopeToken;
 import it.unive.lisa.analysis.SemanticException;
 import it.unive.lisa.analysis.SemanticOracle;
+import it.unive.lisa.analysis.lattices.FunctionalLattice;
+import it.unive.lisa.analysis.lattices.InverseSetLattice;
 import it.unive.lisa.analysis.lattices.Satisfiability;
 import it.unive.lisa.analysis.value.ValueDomain;
 import it.unive.lisa.program.cfg.ProgramPoint;
@@ -12,94 +17,53 @@ import it.unive.lisa.symbolic.value.Identifier;
 import it.unive.lisa.symbolic.value.ValueExpression;
 import it.unive.lisa.util.representation.StructuredRepresentation;
 
-public class EqualityDomain implements ValueDomain<EqualityDomain> {
+public class EqualityDomain extends FunctionalLattice<EqualityDomain, Identifier, EqualityDomain.SetOfIdentifiers> implements ValueDomain<EqualityDomain> {
 
-    @Override
-    public EqualityDomain assign(Identifier id, ValueExpression expression, ProgramPoint pp, SemanticOracle oracle)
-            throws SemanticException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'assign'");
+    public EqualityDomain(SetOfIdentifiers lattice) {
+        super(lattice);
+    }
+
+    public EqualityDomain(SetOfIdentifiers lattice, Map<Identifier, SetOfIdentifiers> function) {
+        super(lattice, function);
     }
 
     @Override
-    public EqualityDomain smallStepSemantics(ValueExpression expression, ProgramPoint pp, SemanticOracle oracle)
-            throws SemanticException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'smallStepSemantics'");
+    public SetOfIdentifiers stateOfUnknown(Identifier key) {
+        // for an unknown variable, assume no equalities: top of SetOfIdentifiers
+        return new SetOfIdentifiers(Collections.emptySet(), true);
     }
 
     @Override
-    public EqualityDomain assume(ValueExpression expression, ProgramPoint src, ProgramPoint dest, SemanticOracle oracle)
-            throws SemanticException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'assume'");
+    public EqualityDomain mk(SetOfIdentifiers lattice, Map<Identifier, SetOfIdentifiers> function) {
+        return new EqualityDomain(lattice, function);
     }
 
-    @Override
-    public boolean knowsIdentifier(Identifier id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'knowsIdentifier'");
-    }
+    // Tracks, for each variable, the set of variables known to be equal to it.
+    // InverseSetLattice: more elements = more equalities known = more precise = lower in lattice.
+    // lub (join at merge) = intersection: keep only equalities that hold in BOTH branches.
+    static class SetOfIdentifiers extends InverseSetLattice<SetOfIdentifiers, Identifier> {
 
-    @Override
-    public EqualityDomain forgetIdentifier(Identifier id) throws SemanticException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'forgetIdentifier'");
-    }
+        public SetOfIdentifiers(Set<Identifier> elements, boolean isTop) {
+            super(elements, isTop);
+        }
 
-    @Override
-    public EqualityDomain forgetIdentifiersIf(Predicate<Identifier> test) throws SemanticException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'forgetIdentifiersIf'");
-    }
+        @Override
+        public SetOfIdentifiers mk(Set<Identifier> set) {
+            // isTop = true only when the set is empty and represents the top element
+            return new SetOfIdentifiers(set, set.isEmpty());
+        }
 
-    @Override
-    public Satisfiability satisfies(ValueExpression expression, ProgramPoint pp, SemanticOracle oracle)
-            throws SemanticException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'satisfies'");
-    }
+        @Override
+        public SetOfIdentifiers top() {
+            // top = empty set with isTop=true: no equality constraints known
+            return new SetOfIdentifiers(Collections.emptySet(), true);
+        }
 
-    @Override
-    public StructuredRepresentation representation() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'representation'");
-    }
-
-    @Override
-    public EqualityDomain pushScope(ScopeToken token) throws SemanticException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'pushScope'");
-    }
-
-    @Override
-    public EqualityDomain popScope(ScopeToken token) throws SemanticException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'popScope'");
-    }
-
-    @Override
-    public boolean lessOrEqual(EqualityDomain other) throws SemanticException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'lessOrEqual'");
-    }
-
-    @Override
-    public EqualityDomain lub(EqualityDomain other) throws SemanticException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'lub'");
-    }
-
-    @Override
-    public EqualityDomain top() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'top'");
-    }
-
-    @Override
-    public EqualityDomain bottom() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'bottom'");
+        @Override
+        public SetOfIdentifiers bottom() {
+            // bottom = empty set with isTop=false: unreachable / contradiction
+            return new SetOfIdentifiers(Collections.emptySet(), false);
+        }
     }
 
 }
